f634dcf0f722be1e2034058ddf9e2e2926513df9
gironziricardos <elingenierorg@hotmail.com>
2022-11-29 22:37:23 -0300

    agregado a api de las  funciones

--- posteo_categorias.php
+++ posteo_categorias.php
@@ -7,4 +7,62 @@ header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers
 
 include_once 'db.php';
 
+//consultar por id_posteocategorias
+if (isset($_GET["consultar"])){
+    $sqlPosteoCategorias = mysqli_query($conexionBD,"SELECT * FROM posteo_categorias WHERE id_posteocategorias=".$_GET["consultar"]);
+    if(mysqli_num_rows($sqlPosteocategorias) > 0){
+        $posteocategorias = mysqli_fetch_all($sqlPosteoCategorias,MYSQLI_ASSOC);
+        echo json_encode($posteocategorias);
+        exit();
+    }
+    else{  echo json_encode(["success"=>0]); }
+}
+
+//borrado de un registro por id_posteocategorias
+if (isset($_GET["borrar"])){
+    $sqlPosteoCategorias = mysqli_query($conexionBD,"DELETE FROM posteo_categorias WHERE id=".$_GET["borrar"]);
+    if($sqlPosteoCategorias){
+        echo json_encode(["success"=>1]);
+        exit();
+    }
+    else{  echo json_encode(["success"=>0]); }
+}
+
+//agregar un posteo
+if(isset($_GET["insertar"])){
+    $data = json_decode(file_get_contents("php://input"));
+    $id_posteocategorias=$data->id_posteocategorias;
+    $id_posteo=$data->id_posteo;
+    $id_categoria=$data->$id_categoria;
+        if(($id_posteocategorias!="")&&($id_posteo!="")&&($id_categoria!="")){
+            
+    $sqlEmpleaados = mysqli_query($conexionBD,"INSERT INTO posteo_categorias(id_posteocategorias,id_posteo,id_catgoria) VALUES('$id_posteocategorias','$id_posteo','$id_categoria') ");
+    echo json_encode(["success"=>1]);
+        }
+    exit();
+}
+
+//actualizar los datos de un posteo
+if(isset($_GET["actualizar"])){
+    
+    $data = json_decode(file_get_contents("php://input"));
+
+    $id_posteocategorias=(isset($data->id_posteocategorias))?$data->id_posteocategorias:$_GET["actualizar"];
+    $id_posteo=$data->id_posteo;
+    $id_categoria=$data->id_categoria;
+    
+    $sqlPostoCategorias = mysqli_query($conexionBD,"UPDATE posteo_categorias SET id_posteo='$id_posteo',id_categoria='$id_categoria' WHERE id_posteocategorias='$id_posteocategorias'");
+    echo json_encode(["success"=>1]);
+    exit();
+}
+
+$sqlPosteoCategorias = mysqli_query($conexionBD,"SELECT * FROM posteo_categorias ");
+if(mysqli_num_rows($sqlPosteoCategorias) > 0){
+    $posteocategorias = mysqli_fetch_all($sqlPosteoCategorias,MYSQLI_ASSOC);
+    echo json_encode($posteocategorias);
+}
+else{ echo json_encode([["success"=>0]]); }
+
+
+
 ?>
