f7f7994bff871083a850371abf197920f7afb5c2
Marisol <gironzimarisol@gmail.com>
2022-12-02 15:54:48 -0300

    lugar

--- lugar.php
+++ lugar.php
@@ -4,6 +4,7 @@ header("Access-Control-Allow-Headers: access");
 header("Access-Control-Allow-Methods: GET,POST");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
 
 include_once 'db.php';
 
