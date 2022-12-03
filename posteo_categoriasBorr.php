28f7863fa8ceca8350d3e0d17f60f67f113673f0
gironziricardos <elingenierorg@hotmail.com>
2022-12-03 11:12:27 -0300

    archivos ins,edit,borr,cons de posteo_categorias

---
+++ posteo_categoriasBorr.php
@@ -0,0 +1,24 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: access');
+header("Access-Control-Allow-Methods: GET,POST, DELETE");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+
+include('bd.php');
+
+$sqlPosteocategorias = mysqli_query($conexionBD,"DELETE FROM posteo_categorias WHERE id_posteocategorias=".$_GET["borrar"]);
+
+if(window.confirm("Esta seguro de eliminar este registro?")){
+    if($sqlPosteocategorias){
+        echo json_encode("POSTEO BORRADO CORRECTAMENTE");
+        
+    }
+    else{  echo json_encode("Algo Falló"); 
+    }
+}
+
+
+
+
+?>
