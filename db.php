4355dd8c2768dd97ae0802d23e67bb814b9eea4a
Emiliano Daian <emilianodaian@gmail.com>
2022-12-06 14:41:55 -0300

    Creo Conexion Nueva con PDO

--- db.php
+++ db.php
@@ -1,7 +1,10 @@
 <?php
 
 // Conecta a la base de datos  con usuario, contraseña y nombre de la BD
-$servidor = "localhost"; $usuario = "instit41_bdComun"; $contrasenia = "#NOJLgl.I=BA"; $nombreBaseDatos = "instit41_quasarBDcomun";
+$servidor = "localhost"; 
+$usuario = "instit41_bdComun"; 
+$contrasenia = "#NOJLgl.I=BA"; 
+$nombreBaseDatos = "instit41_quasarBDcomun";
 $conexionBD = new mysqli($servidor, $usuario, $contrasenia, $nombreBaseDatos);
 
 //fin conexion
--- index.php
+++ index.php
@@ -1,5 +1,14 @@
 <?php
+    include 'conexionBD.php';
 
+    $pdo = new Conexion();
 
-
+    if($_SERVER['REQUEST_METHOD'] == 'GET'){
+        $sql = $pdo->prepare("SELECT * FROM usuarios");
+        $sql->execute();
+        $sql->setFetchMode(PDO::FETCH_ASSOC);
+        header("HTTP/1.1 200 OK");
+        echo json_encode($sql->fetchAll());
+        exit;
+    }
 ?>
