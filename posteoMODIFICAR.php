b48d8a334189eb007bd03c019d125e444ecb9944
SebaChai <chaileluissebastian@gmail.com>
2022-12-13 13:32:56 -0300

    update - posteo

--- posteoMODIFICAR.php
+++ posteoMODIFICAR.php
@@ -2,9 +2,10 @@
 
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: access");
-header("Access-Control-Allow-Methods: GET,POST");
+header("Access-Control-Allow-Methods: GET,POST ,DELETE, UPDATE");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
 
 
     include 'conexionBD.php';
