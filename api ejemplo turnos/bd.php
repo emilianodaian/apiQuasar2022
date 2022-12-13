189051506ed67082e7262666acc69a8fba3ae99d
Emiliano Daian <emilianodaian@gmail.com>
2022-12-13 14:12:28 -0300

    Limpieza API

--- api ejemplo turnos/bd.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$servidor = "localhost:3307"; $usuario = "root"; $contrasenia = ""; $nombreBaseDatos = "cmr";
-$conexionBD = new mysqli($servidor, $usuario, $contrasenia, $nombreBaseDatos);
-
-?>
