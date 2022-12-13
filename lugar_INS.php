852c999ec3c33849a0da422179111da5b7ea6005
Franco094 <frnkkofvano@gmail.com>
2022-12-13 00:57:42 -0300

    lugarpdo

--- lugar_INS.php
+++ lugar_INS.php
@@ -7,17 +7,24 @@ header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers
 header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
 
 include_once 'db.php';
+$pdo = new Conexion();
 
-//Insertar
-$data = json_decode(file_get_contents("php://input"));
 
-    $nombre=$data->nombre;
-        
-            try{
-                $sqlLugar = mysqli_query($conexionBD,"INSERT INTO lugar(nombre) VALUES('$nombre')");
-                echo json_encode("LUGAR REGISTRADO CORRECTAMENTE");
-            }catch(Exception $e){
-                echo json_encode($e->getMessage());
-            }
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+    $sql="INSERT INTO lugar (nombre) 
+    VALUES (:nombre)";
+
+    $stmt = $pdo->prepare($sql);
+    $stmt->bindValue(':nombre', $_POST['nombre']);
+    $stmt->execute();
+
+    $id_lugar=$pdo->lastInsertId();
+
+    if($id_lugar){
+        header("HTTP/1.1 200 OK");
+        echo json_encode($id_lugar);
+    }
+    exit;
+}
 
 ?>
